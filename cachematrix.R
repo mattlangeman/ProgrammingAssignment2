8088fdaa4a5c54d173a83eceb5bc9591fda16f6d
Matt Langeman <matt@langeman.net>
2015-06-20 14:09:05 -0400

    add comments to makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,24 @@
 ## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## Store inverse of x as part of object
     inverse_x <- NULL
+    
+    ## setter function for setting intital matrix
     set <- function(y) {
         x <<- y
+        ## New matrix means inverse must be recalculated
         inverse_x <<- NULL
     }
+    ## getter function for getting intial matrix
     get <- function() x
+    
+    ## setter function for getting matrix inverse
     setinverse <- function(inverse) inverse_x <<- inverse
+    
+    ## getter function for getting matrix inverse
     getinverse <- function() inverse_x
+    
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
